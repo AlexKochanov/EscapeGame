0c21c9ba91f12a216550536e5b46f44b7de3164a
alexg <alexg@104.39.130.133>
2018-04-05 17:39:11 -0400

    Alex Commit 4/5/2018 More Maze

--- EscapeGame/src/Board.java
+++ EscapeGame/src/Board.java
@@ -17,13 +17,16 @@ public class Board extends JPanel
       g.fillRect(RECT_X = 0, RECT_Y = 928, RECT_WIDTH = 1000, RECT_HEIGHT = 25);
       g.fillRect(RECT_X = 958, RECT_Y = 0, RECT_WIDTH = 25, RECT_HEIGHT = 1000);
       //Maze Walls
-      g.fillRect(RECT_X = 60, RECT_Y = 60, RECT_WIDTH = 25, RECT_HEIGHT = 150);
-      g.fillRect(RECT_X = 240, RECT_Y = 240, RECT_WIDTH = 25, RECT_HEIGHT = 150);
-      g.fillRect(RECT_X = 60, RECT_Y = 60, RECT_WIDTH = 150, RECT_HEIGHT = 25);
-      g.fillRect(RECT_X = 180, RECT_Y = 240, RECT_WIDTH = 200, RECT_HEIGHT = 25);
+      g.fillRect(RECT_X = 80, RECT_Y = 80, RECT_WIDTH = 25, RECT_HEIGHT = 350);
+      g.fillRect(RECT_X = 80, RECT_Y = 80, RECT_WIDTH = 150, RECT_HEIGHT = 25);
+      g.fillRect(RECT_X = 200, RECT_Y = 200, RECT_WIDTH = 25, RECT_HEIGHT = 200);
+      g.fillRect(RECT_X = 200, RECT_Y = 200, RECT_WIDTH = 150, RECT_HEIGHT = 25);
+      g.fillRect(RECT_X = 600, RECT_Y = 200, RECT_WIDTH = 25, RECT_HEIGHT = 200);
+      g.fillRect(RECT_X = 450, RECT_Y = 200, RECT_WIDTH = 150, RECT_HEIGHT = 25);
+      g.fillRect(RECT_X = 300, RECT_Y = 0, RECT_WIDTH = 25, RECT_HEIGHT = 100);
+      g.fillRect(RECT_X = 300, RECT_Y = 80, RECT_WIDTH = 400, RECT_HEIGHT = 25);
       g.fillRect(RECT_X = 400, RECT_Y = 400, RECT_WIDTH = 25, RECT_HEIGHT = 550);
-      g.fillRect(RECT_X = 700, RECT_Y = 60, RECT_WIDTH = 25, RECT_HEIGHT = 150);
-      
+      g.fillRect(RECT_X = 700, RECT_Y = 80, RECT_WIDTH = 25, RECT_HEIGHT = 350);
    }
     public Board()
     {
