e1f791fbe05f57ff1cc3d7ebf591a3ac8bcdd3df
alexg <alexg@104.39.206.13>
2018-04-17 16:52:30 -0400

    Alex Commit 4/10/2018

---
+++ EscapeGame/src/Key.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Key {
+    
+    private int x = 750;
+    private int y = 750;
+    
+    public Key()
+    {
+        
+    }
+}
